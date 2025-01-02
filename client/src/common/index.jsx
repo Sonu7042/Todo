683bd12110e11b0996b353a0e1fb2e82593d5468
Sonu7042 <sonusinghmourya100@gmail.com>
2025-01-02 20:48:45 +0530

    updated

---
+++ client/src/common/index.jsx
@@ -0,0 +1,37 @@
+const host= 'http://localhost:9000'
+// const host= "https://resume-project-fawn.vercel.app"
+
+const SummeryApi={
+
+    signup:{
+        url: `${host}/signup`,
+        method : 'post'
+    },
+
+    login:{
+        url: `${host}/login`,
+        method : 'post'
+    },
+    add:{
+        url: `${host}/add`,
+        method: "post"
+    },
+    show:{
+        url: `${host}/show`,
+        method : "get"
+    },
+
+    delete:{
+        url: `${host}/delete`,
+        method : "delete"
+    },
+
+    update:{
+        url: `${host}/update`,
+        method: "post"
+    }
+
+}
+
+
+export default SummeryApi
